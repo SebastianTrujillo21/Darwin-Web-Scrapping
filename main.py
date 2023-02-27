6e36c350c146c70548fa980296e75d103a610c6a
Sebastian <ippo_sebas@hotmail.com>
2023-02-27 10:20:54 -0500

    Finish and my last proyect in GSI

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
-import numpy as np
+import tkinter
+from tkinter import filedialog
 import pandas as pd
 from openpyxl import load_workbook
 from selenium import webdriver
@@ -7,10 +8,53 @@ from selenium.common.exceptions import NoSuchElementException, ElementClickInter
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
-username = "30030644"
-password = "T0Ea2SxG"
 
-filename = "DNI.xlsx"
+def menu_registro_usuario():
+    global nombre_usuario_entry
+    global contrasena_usuario_entry
+    global nombre_usuario_verify
+    global contrasena_usuario_verify
+    global dirExcel
+    global pantalla
+    pantalla = tkinter.Tk()
+    pantalla.geometry("300x380")
+    pantalla.title("Inicio de Sesion")
+    tkinter.Label(pantalla, text="").pack()
+    image = tkinter.PhotoImage(file="GSI.gif")
+    image = image.subsample(2, 2)
+    tkinter.Label(pantalla, image=image).pack()
+    tkinter.Label(pantalla, text="").pack()
+
+    nombre_usuario_verify = tkinter.StringVar()
+    contrasena_usuario_verify = tkinter.StringVar()
+    dirExcel = tkinter.StringVar()
+
+    tkinter.Label(pantalla, text="Username").pack()
+    nombre_usuario_entry = tkinter.Entry(pantalla, textvariable=nombre_usuario_verify)
+    nombre_usuario_entry.pack()
+    tkinter.Label(pantalla, text="").pack()
+
+    tkinter.Label(pantalla, text="Password").pack()
+    contrasena_usuario_entry = tkinter.Entry(pantalla, textvariable=contrasena_usuario_verify)
+    contrasena_usuario_entry.pack()
+    tkinter.Label(pantalla, text="").pack()
+
+    tkinter.Button(pantalla, text="Iniciar Sesion", command=abrirArchivo).pack()
+    pantalla.mainloop()
+
+
+def abrirArchivo():
+    archivo = filedialog.askopenfilename(initialdir="/", title="Seleccionar el archivo",
+                                         filetypes=(("xlsx files", "*.xlsx"), ("all files", "*.*")))
+    dirExcel.set(archivo)
+    pantalla.destroy()
+
+
+menu_registro_usuario()
+username = nombre_usuario_verify.get()  # Usuario: 30030644
+password = contrasena_usuario_verify.get()  # Clave: 30644feb
+
+filename = str(dirExcel.get())
 filesheet = pd.read_excel(filename, usecols='A')
 workbook = load_workbook(filename)
 worksheet = workbook.active
@@ -21,6 +65,10 @@ primerApellidoArray = []
 segundoApellidoArray = []
 correoArray = []
 telefonoArray = []
+cuentaBancariaArray = []
+cupsLuzArray = []
+cupsGasArray = []
+direccionArray = []
 
 
 def tarea():
@@ -29,7 +77,7 @@ def tarea():
     driver.find_element(By.XPATH, "/html/body/div[1]/section/div[2]/div/div/button").click()
     time.sleep(10)
     driver.find_element(By.XPATH,
-                        "/html/body/div/div/div[2]/form/div/div/div/div/div[2]/div[2]/span/div/div/div/div/div/div/div/div/a/div[2]").click()
+                        '//*[@id="auth0-lock-container-1"]/div/div[2]/form/div/div/div/div/div[2]/div[2]/span/div/div/div/div/div/div/div/div/a/div[2]').click()
     time.sleep(10)
     driver.find_element(By.XPATH, "/html/body/div/div/div/div[2]/div[1]/form/input[3]").send_keys(username)
     driver.find_element(By.XPATH, "/html/body/div/div/div/div[2]/div[1]/form/input[4]").send_keys(password)
@@ -48,21 +96,47 @@ def tarea():
     driver.find_element(By.XPATH, "/html/body/div[1]/section/div[2]/div/div/div/div[3]/button").click()
     time.sleep(10)
     for i in range(len(dniArray)):
-        driver.find_element(By.XPATH,
-                            "/html/body/div[1]/section/div[2]/div/div/div[3]/div[1]/div[2]/div[1]/div/div/div/div/form/div[1]/div/div[2]/div/div/input").send_keys(
-            dniArray[i])
         time.sleep(5)
-        driver.find_element(By.XPATH,
-                            "/html/body/div[1]/section/div[2]/div/div/div[3]/div[1]/div[2]/div[1]/div/div/div/div/form/div[1]/div/div[3]/button").click()
+        try:
+            driver.find_element(By.XPATH,
+                                "/html/body/div[1]/section/div[2]/div/div/div[3]/div[1]/div[2]/div[1]/div/div/div/div/form/div[1]/div/div[2]/div/div/input").send_keys(
+                dniArray[i])
+        except NoSuchElementException:
+            driver.refresh()
         time.sleep(10)
+
+        try:
+            driver.find_element(By.XPATH,
+                                "/html/body/div[1]/section/div[2]/div/div/div[3]/div[1]/div[2]/div[1]/div/div/div/div/form/div[1]/div/div[3]/button").click()
+        except ElementClickInterceptedException:
+            driver.refresh()
+        time.sleep(10)
+
         try:
             driver.find_element(By.XPATH,
                                 "/html/body/div[6]/div[3]/div/div[2]/div[2]/div[1]/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div/div[1]/span/span/input").click()
+            texto_largo = driver.find_element(By.XPATH, "//div[@columns='[object Object]']").text
+            textoLargoArray = texto_largo.split("\n")
+            direccionArray.append(textoLargoArray[0])
+            for j, contenido in enumerate(textoLargoArray):
+                tamanoArrayCupLuz = len(cupsLuzArray)
+
+                if textoLargoArray[j] == "CUPS LUZ":
+                    cupsLuzArray.append(textoLargoArray[j + 1])
+
+                if textoLargoArray[j] == "CUPS GAS":
+                    cupsGasArray.append(textoLargoArray[j + 1])
+
+                if j > 0:
+                    if len(cupsLuzArray) > tamanoArrayCupLuz:
+                        cupsGasArray.append(" ")
+
         except NoSuchElementException:
             driver.refresh()
             continue
 
         time.sleep(5)
+
         try:
             driver.find_element(By.XPATH, "/html/body/div[6]/div[3]/div/div[2]/div[2]/div[2]/button").click()
         except NoSuchElementException:
@@ -70,6 +144,7 @@ def tarea():
             continue
 
         time.sleep(5)
+
         try:
             nombre = driver.find_element(By.XPATH,
                                          "/html/body/div[1]/section/div[2]/div/div/div[3]/div[1]/div[2]/div[1]/div/div/div/div/form/div[1]/div/div[4]/div/div/input")
@@ -94,16 +169,44 @@ def tarea():
         correoArray.append(text_correo)
         telefonoArray.append(text_telefono)
 
+        # DATOS DE CUENTA BANCARIA
+        time.sleep(10)
+
+        driver.find_element(By.XPATH,
+                            "//body[1]/div[1]/section[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div[2]/div[5]/span[1]/span[1]/span[1]").click()
+
+        time.sleep(5)
+        driver.find_element(By.XPATH, "//form[@autocomplete='off']//button[@type='submit']").click()
+
+        try:
+            cuentaBancaria = driver.find_element(By.XPATH,
+                                                 "//body/div[@id='root']/section[@role='main']/div/div/div/div/div/div/div/div/div[2]").text
+
+            cuentaBancariaArray.append(cuentaBancaria[16:len(cuentaBancaria)])
+        except NoSuchElementException:
+            print("No hay cuenta bancaria")
+
         for a, value in enumerate(nombreArray):
-            worksheet.cell(row=i+2, column=2, value=value)
+            worksheet.cell(row=i + 2, column=2, value=value)
         for a, value in enumerate(primerApellidoArray):
-            worksheet.cell(row=i+2, column=3, value=value)
+            worksheet.cell(row=i + 2, column=3, value=value)
         for a, value in enumerate(segundoApellidoArray):
-            worksheet.cell(row=i+2, column=4, value=value)
+            worksheet.cell(row=i + 2, column=4, value=value)
         for a, value in enumerate(correoArray):
-            worksheet.cell(row=i+2, column=5, value=value)
+            worksheet.cell(row=i + 2, column=5, value=value)
         for a, value in enumerate(telefonoArray):
-            worksheet.cell(row=i+2, column=6, value=value)
+            worksheet.cell(row=i + 2, column=6, value=value)
+        for a, value in enumerate(cuentaBancariaArray):
+            worksheet.cell(row=i + 2, column=7, value=value)
+        for a, value in enumerate(direccionArray):
+            worksheet.cell(row=i + 2, column=8, value=value)
+        for a, value in enumerate(cupsLuzArray):
+            worksheet.cell(row=i + 2, column=9, value=value)
+        for a, value in enumerate(cupsGasArray):
+            worksheet.cell(row=i + 2, column=10, value=value)
+
+        print(cupsLuzArray)
+        print(cupsGasArray)
 
         workbook.save(filename)
         driver.refresh()
